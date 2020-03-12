0f90653f1f455c2b85065be12e046a2524c54a49
Alfarel Zaki <alfarell.zaki@mail.com>
2020-03-12 10:13:24 +0700

    inisialisasi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Col, Row } from 'antd';
 
 function App() {
   
+  // data untuk dimasukkan pada card
   const cardData = [
     {
       title: "Alfarel Zaki",
@@ -18,16 +20,26 @@ function App() {
       desc: "Fullstack Developer Kae"
     },
   ]
+
+  const CardAnt = ()=> {
+    return(
+      <div className="site-card-wrapper">
+        <Row gutter={16}>
+          {cardData.map (data =>
+            <Col span={6}>
+              <Card title={data.title} extra={<a href="#">More</a>} style={{ width: 300 }}>
+                <p className="desc">{data.desc}</p>
+              </Card>
+            </Col>
+          )}
+        </Row>
+      </div>
+    )
+  }
   
+  // render component card, row, dan column
   return (
-    <div>
-      {cardData.map (data =>
-        <div className="card">
-          <p className="title">{data.title}</p>
-          <p className="desc">{data.desc}</p>
-        </div>
-      )}
-    </div>
+    <CardAnt/>
   );
 }
 
